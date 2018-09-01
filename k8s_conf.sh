f0947c175f7eb46227513b5bf662c377c8e8b1e2
EvgenyPodshivalov <a@stelsik.ru>
2018-09-01 21:25:31 +0300

    update

--- k8s_conf.sh
+++ k8s_conf.sh
@@ -2,21 +2,24 @@
 
 LabelKey=app
 LabelSvc=kibana
-ExposePort=30355
+ExposePort=30355 8022
 LocalPort=8200
 
 KubeIPs=$(kubectl get nodes -o wide | awk 'match($3,/(.*)worker(.*)/){print $6}')
 
 ServerNum=0
 
-for LbIP in $KubeIPs
+for port in "${ExposePort[@]}"
 do
-    ((ServerNum++))
-    LbString="  server LB$ServerNum $LbIP:$ExposePort check"
-    LoadBalancer="$LbString\n$LoadBalancer"
+    for LbIP in $KubeIPs
+    do
+        ((ServerNum++))
+        LbString="  server LB$ServerNum $LbIP:$ExposePort check"
+        LoadBalancer="$LbString\n$LoadBalancer"
+    done
 done
 
 sed "s/BACKEND_LIST/$LoadBalancer/g" /etc/haproxy/haproxy.cfg.config | \
 sed "s/BACKEND_PORT/$LocalPort/g" > /etc/haproxy/haproxy.cfg
 
-service haproxy reload
+#service haproxy reload
