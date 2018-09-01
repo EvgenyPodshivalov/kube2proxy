e8cd758633199c1a1934c1729f49976b4363fcb0
WebDev Server <evgeniy.podshivalov@ofd-ya.ru>
2018-09-01 21:35:32 +0300

    upd

--- k8s_conf.sh
+++ k8s_conf.sh
@@ -2,19 +2,19 @@
 
 LabelKey=app
 LabelSvc=kibana
-ExposePort=30355 8022
+ExposePort=(30355 8022 999)
 LocalPort=8200
 
 KubeIPs=$(kubectl get nodes -o wide | awk 'match($3,/(.*)worker(.*)/){print $6}')
 
 ServerNum=0
 
-for port in "${ExposePort[@]}"
+for Port in "${ExposePort[@]}"
 do
     for LbIP in $KubeIPs
     do
         ((ServerNum++))
-        LbString="  server LB$ServerNum $LbIP:$ExposePort check"
+        LbString="  server LB$ServerNum $LbIP:$Port check"
         LoadBalancer="$LbString\n$LoadBalancer"
     done
 done
