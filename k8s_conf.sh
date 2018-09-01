14d65a2830cc6ea852c581a5c7b02a179d52e72c
EvgenyPodshivalov <36457612+EvgenyPodshivalov@users.noreply.github.com>
2018-09-01 19:30:42 +0300

    Update k8s_conf.sh

--- k8s_conf.sh
+++ k8s_conf.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-#LB_IPs=`kubectl describe ingress --all-namespaces | awk '{if ($1 == "Address:"){print $2}}' | head -1`
-#LB_IPs=`kubectl get ingress --all-namespaces | awk '{if ($3 != "HOSTS" || $4 != "ADDRESS"){print $3,$4}}'`
-#KubeIPs=`kubectl describe pods --namespace ingress-nginx | awk '{if ($1 == "IP:"){print $2}}'`
-#s=`kubectl get pods --namespace ingress-nginx | awk 'match($1, /nginx-ingress-controller(.*)/){print $1}'`
 
 Key=app
 Svc=kibana
 ExposePort=30355
 LocalPort=8200
-
-#KubeIPs=$(kubectl describe pods -l $Key=$Svc --all-namespaces | awk '{if($1 == "Node:"){ print $2 }}' | awk 'match($0, /(.*)\/(.*)/, m){print m[2]}')
 
 KubeIPs=$(kubectl get nodes -o wide | awk 'match($3,/(.*)worker(.*)/){print $6}')
 
